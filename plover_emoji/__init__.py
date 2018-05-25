359866d1a9eafd68292688efaf29978c8210fa2a
Ted Morin <morinted@gmail.com>
2018-05-25 15:21:21 -0400

    First working implementation

--- plover_emoji/__init__.py
+++ plover_emoji/__init__.py
@@ -1,24 +1,34 @@
 import json
 import re
-from fuzzywuzzy import process, fuzz
 from fuzzyset import FuzzySet
-from pprint import pprint
+import pkg_resources
+
+EMOJI_WORD_WHITELIST = re.compile('[A-Za-z0-10:\\-!? ]+\\Z')
 
 def make_tokens(emoji_strategy: dict):
     name_to_unicode_output = {}
     longest_name = ''
     for _, emoji in emoji_strategy.items():
-        name = re.sub("[^A-Za-z ]", "", emoji['name'])  
+        name = re.sub("[^A-Za-z0-10 ]", "", emoji['name']) # Remove symbols.
+        name = re.sub(" +", " ", name) # Remove repeated spaces.
         unicode_output = emoji['unicode_output']
         # :sweat_smile: --> sweat smile
         shortname = ' '.join(emoji['shortname'][1:-1].split('_'))
 
         name_to_unicode_output[name] = unicode_output
         name_to_unicode_output[shortname] = unicode_output
-    print(longest_name)
+
+    # Manual aliases
+    name_to_unicode_output['!'] = name_to_unicode_output['exclamation mark']
+    name_to_unicode_output['?'] = name_to_unicode_output['question mark']
+    name_to_unicode_output['!!'] = name_to_unicode_output['double exclamation mark']
+    name_to_unicode_output['!?'] = name_to_unicode_output['interrobang']
+    name_to_unicode_output['?!'] = name_to_unicode_output['interrobang']
     return name_to_unicode_output
 
-with open('./emoji_strategy.json') as f:
+emoji_file = pkg_resources.resource_filename('plover_emoji', 'emoji_strategy.json')
+
+with open(emoji_file) as f:
     data = json.load(f)
     name_to_unicode_output = make_tokens(data)
     fuzzy_emoji_set = FuzzySet(name_to_unicode_output.keys())
@@ -30,25 +40,22 @@ def unicode_output_to_characters(unicode_output):
     return ''.join([ chr(int(code, 16)) for code in unicode_output.split('-') ])
 
 def find_emoji_by_phrase(phrase):
+    if isinstance(phrase, str): # Convert from string to list for unit tests.
+        phrase = [word + ' ' for word in phrase.split(' ') if word]
+        phrase[-1] = phrase[-1][:-1] # Remove final trailing space.
     matches = []
     search = []
-    for word in reversed(phrase.split(' ')):
+    for word in reversed(phrase):
         search = [word] + search
-        query = ' '.join(search)
-        # matches.append(
-        #    process.extract(query, name_to_unicode_output.keys(), limit=1, scorer=fuzz.token_sort_ratio)[0] + (query,)
-        # )
-        best_match = fuzzy_emoji_set.get(query)
+        query = ''.join(search)
+        best_match = fuzzy_emoji_set.get(query.strip())
         if best_match:
-            print('matches')
-            pprint(best_match)
             matches.append(
                 best_match[0] + (query,)
             )
     highscore = 0
     emoji = None
     words = None
-    pprint(matches)
     for match in matches:
         percentage, name, query = match
         percentage *= 100
@@ -57,17 +64,34 @@ def find_emoji_by_phrase(phrase):
             highscore = classifier
             emoji = name
             words = query
-    if highscore > 50:
-        print('winner is', (unicode_output_to_characters(get_emoji(emoji)), words))
+    if highscore > 70:
         return (unicode_output_to_characters(get_emoji(emoji)), words)
     return None
 
+def get_emoji_phrase(words):
+    emoji_phrase = []
+    counter = 0
+    for word in words:
+        # We don't really want emoji to span across line breaks or sentences
+        if not EMOJI_WORD_WHITELIST.match(word):
+            break
+        emoji_phrase.append(word)
+        counter += 1
+        # 10 words should be enough for all of the emoji.
+        if counter >= 10:
+            break
+    return list(reversed(emoji_phrase))
+
 def emoji(ctx, cmdline):
     # Placeholder
-    action = ctx.new_action()
-    for word in ctx.iter_last_words():
-        print('dorp: ', word)
-        return action
+    action = ctx.copy_last_action()
+    result = find_emoji_by_phrase(get_emoji_phrase(ctx.iter_last_words()))
+    if result:
+        emoji, phrase = result
+        action.text = emoji
+        action.prev_replace = ''.join(phrase)
+        action.prev_attach = True
+        action.word = None
     return action
 
 if __name__ == '__main__':
--- test/test_plover_emoji.py
+++ test/test_plover_emoji.py
@@ -1,4 +1,11 @@
-from plover_emoji import unicode_output_to_characters, get_emoji, find_emoji_by_phrase
+import sys
+sys.path.append('..')
+from plover_emoji import (
+    unicode_output_to_characters,
+    get_emoji,
+    find_emoji_by_phrase,
+    get_emoji_phrase,
+)
 
 def test_unicode_output_to_characters():
     assert unicode_output_to_characters('1f605') == '😅'
@@ -6,12 +13,14 @@ def test_unicode_output_to_characters():
     assert unicode_output_to_characters('1f468-200d-1f469-200d-1f466-200d-1f466') == '👨‍👩‍👦‍👦'
 
 def test_get_emoji():
-    assert get_emoji('smiling face with open mouth & cold sweat') == '1f605'
+    # "open mouth & cold sweat" → "open mouth cold sweat"
+    assert get_emoji('smiling face with open mouth cold sweat') == '1f605'
+
     assert get_emoji('sweat smile') == '1f605'
-    assert get_emoji('family: man, woman, boy, boy') == '1f468-200d-1f469-200d-1f466-200d-1f466'
+    assert get_emoji('family man woman boy boy') == '1f468-200d-1f469-200d-1f466-200d-1f466'
     assert get_emoji('family mwbb') == '1f468-200d-1f469-200d-1f466-200d-1f466'
 
-def test_find_emoji_by_phrase():
+def test_find_emoji_by_phrase_success():
     assert find_emoji_by_phrase('I had a sweat smile') == ('😅', 'sweat smile')
     assert find_emoji_by_phrase('she was a woman police officer') == ('👮‍♀️', 'woman police officer')
     assert find_emoji_by_phrase('she was a woman police officer tone 1') == ('👮🏻‍♀️', 'woman police officer tone 1')
@@ -22,6 +31,7 @@ def test_find_emoji_by_phrase():
     assert find_emoji_by_phrase('omg I am crying laughing') == ('😆', 'laughing')
     assert find_emoji_by_phrase('time for coffee') == ('☕', 'coffee')
     assert find_emoji_by_phrase('some cool kissing heart') == ('😘', 'kissing heart')
+    assert find_emoji_by_phrase('pop some ta-da') == ('🎉', 'ta-da')
     assert find_emoji_by_phrase('pop some confetti') == ('🎊', 'confetti')
     assert find_emoji_by_phrase('I have a family woman woman boy') == ('👩‍👩‍👦', 'family woman woman boy')
     assert find_emoji_by_phrase('grin') == ('😁', 'grin')
@@ -33,6 +43,38 @@ def test_find_emoji_by_phrase():
     assert find_emoji_by_phrase('v') == ('✌️', 'v')
     assert find_emoji_by_phrase('victory hand') == ('✌️', 'victory hand')
     assert find_emoji_by_phrase('okay hand') == ('👌', 'okay hand')
-    
+    assert find_emoji_by_phrase('what time is clock 4') == ('🕓', 'clock 4')
+    assert find_emoji_by_phrase('guess what time it is clock 4:30') == ('🕟', 'clock 4:30')
+    assert find_emoji_by_phrase('one-two punch punch') == ('👊', 'punch')
+    assert find_emoji_by_phrase('he made a hand signal sign of the horns') == ('🤘', 'sign of the horns')
+    assert find_emoji_by_phrase('I must not see no evil') == ('🙈', 'see no evil')
+
     # Too far from the name:
     assert find_emoji_by_phrase('some cool heart eyes kissing') == ('😗', 'kissing')
+
+
+def test_find_emoji_by_phrase_failure():
+    # 75% similar so a false positive.
+    assert find_emoji_by_phrase('nope') == ('👃', 'nope')
+    # Too far off
+    assert find_emoji_by_phrase('garbage') == None
+    assert find_emoji_by_phrase('enchilada') == None
+
+def test_find_emoji_by_phrase_words():
+    assert find_emoji_by_phrase(['?']) == ('❓', '?')
+    assert find_emoji_by_phrase(['!']) == ('❗', '!')
+    assert find_emoji_by_phrase([' you', ' are', ' a', ' dog']) == ('🐕', ' dog')
+
+def test_get_emoji_phrase():
+    # Don't process other emoji
+    assert get_emoji_phrase(['dog', '😁']) == ['dog']
+    # Don't process newlines
+    assert get_emoji_phrase([' cat', 'this\n' ]) == [' cat']
+
+    # Colon is okay
+    family = [' family', ':', ' man', ' woman', ' boy']
+    assert get_emoji_phrase(family) == list(reversed(family))
+
+    # Only process the last ten words (note that generator is from last word, so result is reversed)
+    assert get_emoji_phrase([ 'a' ] * 50) == [ 'a' ] * 10
+    assert get_emoji_phrase([ 'a' ] * 10 + [ 'b' ] * 10) == [ 'a' ] * 10
